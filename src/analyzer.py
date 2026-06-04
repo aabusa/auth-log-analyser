edf3e70f8c7fc492e55ee370b12d65c29ec87787
aabusa <156257837+aabusa@users.noreply.github.com>
2026-06-04 18:09:51 +0100

    added count to find the exact number of failed attempts and also formatted the output

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,10 +1,13 @@
+from collections import Counter
+ip_counts = Counter()
 with open("data/sample_auth.log" , "r") as log_data :
     for line in log_data :
         if "Failed" in line :
             parts = line.split()
             ip_index = parts.index("from") + 1
             ip = parts[ip_index]
-            print("Failed ip is ", ip)
+            ip_counts[ip] += 1
             
-            
-            
+ranked_ips = ip_counts.most_common()
+for ip, count in ranked_ips:
+    print(f"{ip}  —  {count} failed attempts")        
